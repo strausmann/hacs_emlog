c8af2fba8c0587f78bf90f05939094174e364226
Björn Strausmann <bjoern@strausmann.net>
2026-01-14 13:59:07 +0100

    Add constants for emlog configuration

---
+++ custom_components/emlog/const.py
@@ -0,0 +1,12 @@
+DOMAIN = "emlog"
+
+CONF_HOST = "host"
+CONF_STROM_INDEX = "strom_index"
+CONF_GAS_INDEX = "gas_index"
+CONF_SCAN_INTERVAL = "scan_interval"
+
+DEFAULT_STROM_INDEX = 1
+DEFAULT_GAS_INDEX = 2
+DEFAULT_SCAN_INTERVAL = 30 # seconds
+
+EMLOG_EXPORT_PATH = "/pages/getinformation.php"
