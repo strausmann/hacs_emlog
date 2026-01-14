8939d9379876424c88cd6788f7e8eabfda07c248
Björn Strausmann <bjoern@strausmann.net>
2026-01-14 22:54:18 +0000

    feat(const): add constants for monthly advance and settlement month configuration

--- custom_components/emlog/const.py
+++ custom_components/emlog/const.py
@@ -2,8 +2,7 @@ DOMAIN = "emlog"
 
 # Config
 CONF_HOST = "host"
-CONF_STROM_INDEX = "strom_index"
-CONF_GAS_INDEX = "gas_index"
+CONF_METER_INDEX = "meter_index"
 CONF_PRICE_KWH = "price_kwh"
 CONF_GAS_BRENNWERT = "gas_brennwert"
 CONF_GAS_ZUSTANDSZAHL = "gas_zustandszahl"
@@ -14,6 +13,11 @@ CONF_BASE_PRICE_STROM = "base_price_strom"
 CONF_BASE_PRICE_GAS = "base_price_gas"
 CONF_BASE_PRICE_STROM_HELPER = "base_price_strom_helper"
 CONF_BASE_PRICE_GAS_HELPER = "base_price_gas_helper"
+CONF_MONTHLY_ADVANCE_STROM = "monthly_advance_strom"
+CONF_MONTHLY_ADVANCE_GAS = "monthly_advance_gas"
+CONF_MONTHLY_ADVANCE_STROM_HELPER = "monthly_advance_strom_helper"
+CONF_MONTHLY_ADVANCE_GAS_HELPER = "monthly_advance_gas_helper"
+CONF_SETTLEMENT_MONTH = "settlement_month"
 CONF_METER_TYPE = "meter_type"  # "strom" oder "gas"
 CONF_SCAN_INTERVAL = "scan_interval"
 
@@ -31,5 +35,8 @@ DEFAULT_GAS_BRENNWERT = 11.58
 DEFAULT_GAS_ZUSTANDSZAHL = 0.95
 DEFAULT_BASE_PRICE_STROM = 0.0  # EUR/Monat
 DEFAULT_BASE_PRICE_GAS = 0.0    # EUR/Monat
+DEFAULT_MONTHLY_ADVANCE_STROM = 0.0  # EUR/Monat
+DEFAULT_MONTHLY_ADVANCE_GAS = 0.0    # EUR/Monat
+DEFAULT_SETTLEMENT_MONTH = 12  # Dezember (Monat 12)
 
 EMLOG_EXPORT_PATH = "/pages/getinformation.php"
